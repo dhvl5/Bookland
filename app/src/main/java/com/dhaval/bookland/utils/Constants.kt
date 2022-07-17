41f12f2a938f4894a454a90fc597d8f1b9a871ec
Dhaval Prajapati <prajapatidhaval304@gmail.com>
2022-07-17 21:14:24 +0530

    Added Constants

---
+++ app/src/main/java/com/dhaval/bookland/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.dhaval.bookland.utils
+
+class Constants {
+    companion object {
+        const val DB_NAME = "BookDB"
+        const val BACKED_UP_FILE_NAME = "bookland-backup.db"
+    }
+}
