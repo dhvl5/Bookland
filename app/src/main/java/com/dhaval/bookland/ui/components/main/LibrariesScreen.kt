8c576bf2722f5cfe1ba153fb2d02ae4a71a1045b
Dhaval Prajapati <prajapatidhaval304@gmail.com>
2022-07-17 21:21:49 +0530

    Added Libraries screen

---
+++ app/src/main/java/com/dhaval/bookland/ui/components/main/LibrariesScreen.kt
@@ -0,0 +1,86 @@
+package com.dhaval.bookland.ui.components.main
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.google.accompanist.insets.ui.Scaffold
+import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
+
+@Composable
+fun LibrariesScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = { LibrariesTopBar(navController) },
+    ) { contentPadding ->
+        LibrariesContainer(
+            modifier = Modifier.padding(contentPadding),
+            showLicenseBadges = false,
+        )
+    }
+}
+
+@Composable
+fun LibrariesTopBar(navController: NavHostController) {
+    val appBarHorizontalPadding = 4.dp
+    val titleIconModifier = Modifier
+        .fillMaxHeight()
+        .width(72.dp - appBarHorizontalPadding)
+
+    TopAppBar(
+        modifier = Modifier.fillMaxWidth(),
+        backgroundColor = MaterialTheme.colors.background,
+        elevation = 0.dp,
+    ) {
+        Box(Modifier.height(32.dp)) {
+            Row(titleIconModifier, verticalAlignment = Alignment.CenterVertically) {
+                CompositionLocalProvider(
+                    LocalContentAlpha provides ContentAlpha.high,
+                ) {
+                    IconButton(
+                        onClick = {
+                            navController.popBackStack()
+                        },
+                        enabled = true,
+                    ) {
+                        Icon(
+                            Icons.Default.ArrowBack,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onPrimary,
+                        )
+                    }
+                }
+            }
+            Row(
+                Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                ProvideTextStyle(value = MaterialTheme.typography.h6) {
+                    CompositionLocalProvider(
+                        LocalContentAlpha provides ContentAlpha.high,
+                    ) {
+                        Text(
+                            text = "Open Source Licenses",
+                            color = MaterialTheme.colors.onPrimary,
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                            ),
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .wrapContentSize(Alignment.Center),
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
