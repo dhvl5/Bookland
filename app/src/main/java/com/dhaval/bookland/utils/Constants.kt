af0df3039bedf654e54094536549d313183e39a6
Dhaval Prajapati <prajapatidhaval304@gmail.com>
2022-07-23 23:34:31 +0530

    Added github and twitter urls

--- app/src/main/java/com/dhaval/bookland/utils/Constants.kt
+++ app/src/main/java/com/dhaval/bookland/utils/Constants.kt
@@ -4,5 +4,7 @@ class Constants {
     companion object {
         const val DB_NAME = "BookDB"
         const val BACKED_UP_FILE_NAME = "bookland-backup.db"
+        const val GITHUB_URL = "https://github.com/dhvl5/Bookland"
+        const val TWITTER_URL = "https://twitter.com/dhvl05"
     }
 }
