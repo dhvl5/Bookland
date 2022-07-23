22e0d49f28940d8a297565f8e41d1583938b0197
Dhaval Prajapati <prajapatidhaval304@gmail.com>
2022-07-23 23:54:14 +0530

    Added animation when the card is pressed

--- app/src/main/java/com/dhaval/bookland/ui/components/search/BookItemCard.kt
+++ app/src/main/java/com/dhaval/bookland/ui/components/search/BookItemCard.kt
@@ -1,9 +1,12 @@
 package com.dhaval.bookland.ui.components.search
 
+import androidx.compose.animation.core.animateFloatAsState
 import com.dhaval.bookland.models.Items
 import com.dhaval.bookland.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,9 +14,10 @@ import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
@@ -30,14 +34,32 @@ fun BookItemCard(item: Items?, onClick: () -> Unit) {
         verticalAlignment = Alignment.CenterVertically,
     ) {
         val imageLinks = item?.volumeInfo?.imageLinks
+        val selected = remember { mutableStateOf(false) }
+        val interactionSource = remember { MutableInteractionSource() }
+        val pressedState by interactionSource.collectIsPressedAsState()
+        val scale = animateFloatAsState(if (selected.value && pressedState) .95f else 1f)
+
+        if (pressedState) {
+            selected.value = true
+
+            DisposableEffect(Unit) {
+                onDispose {
+                    selected.value = false
+                }
+            }
+        }
 
         Card(
             modifier = Modifier
                 .fillMaxWidth()
                 .size(0.dp, 150.dp)
-                .clickable {
-                    onClick()
-                },
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null,
+                    enabled = true,
+                    onClick = { onClick() }
+                )
+                .scale(scale.value),
             elevation = 2.dp,
             backgroundColor = MaterialTheme.colors.surface,
             shape = RoundedCornerShape(corner = CornerSize(16.dp)),
